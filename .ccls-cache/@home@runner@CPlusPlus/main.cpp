a42e8bf5568bace7f9bbed67a2fdd7518034a081
Nakesha Glover <47555790-nakeshaglover@users.noreply.replit.com>
2025-09-13 15:13:54 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@CPlusPlus/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  char first, middle, last;
+  // store your initials in these variables
+  first = 'N';
+  middle = 'D';
+  last = 'G';
+
+  cout << first << middle << last;
+  return 0;
+}
