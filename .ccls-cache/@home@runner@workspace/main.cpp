53bbda15ea604b4dd86dd5a001b67bffd8f36e91
nakeshaglover <nakesha.glover@gmail.com>
2025-09-16 16:19:07 +0000

    main cpp correction

--- .ccls-cache/@home@runner@workspace/main.cpp
+++ .ccls-cache/@home@runner@workspace/main.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
 
-int main() { std::cout << "Hello World!\n"; }
+using namespace std;
+
+int main() 
+{
+  char first, middle, last;
+  // store your initials in these variables
+  first = 'N';
+  middle = 'D';
+  last = 'G';
+
+  cout << first << middle << last;
+  return 0;
+}
